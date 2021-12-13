88b62e897009339e00d06cbb833d6db83bbb54de
pravink94 <pravin.k@prometteur.in>
2021-12-13 17:28:04 +0530

    navigation added

--- src/App.js
+++ src/App.js
@@ -4,16 +4,31 @@ import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 
+
 import {
   LoginScreen, RegistrationScreen
 } from './screens';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const { Navigator, Screen } = createStackNavigator();
+
 class App extends Component{
   render() {
     const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
     return (
       <Provider store={store}>
-        {/* <LoginScreen /> */}
-        <RegistrationScreen />
+        <NavigationContainer>
+          <Navigator
+            screenOptions={{
+              headerShown: false,
+            }}
+            initialRouteName={"LoginScreen"} >
+            <Screen name='LoginScreen' component={LoginScreen} />
+            <Screen name='RegistrationScreen' component={RegistrationScreen} />
+            </Navigator>
+            </NavigationContainer>
       </Provider>
     );
   }
